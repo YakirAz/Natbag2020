1b3ff78b0b0cf0f0293f5f12fdfe2b1bba9a222d
Gadi <62388856+Gadiler@users.noreply.github.com>
2020-05-27 09:16:02 +0300

    Update Natbag.java

--- DevTools/src/class1/Natbag.java
+++ DevTools/src/class1/Natbag.java
@@ -3,68 +3,85 @@ package class1;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class Natbag {
 
-	private flights[] flights;
+	protected flights[] allFlights;
 	private int numOfFlights = 0;
-	private static int flightNumGen;
-	private int MAX_NUM_OF_FLIGHTS = 100;
+	private final int MAX_NUM_OF_FLIGHTS = 100;
 
 	public Natbag() {
-		this.flights = new flights[MAX_NUM_OF_FLIGHTS];
-		this.numOfFlights = flightNumGen++;
+		this.allFlights = new flights[MAX_NUM_OF_FLIGHTS];
 	}
 
-	public boolean addFlight(flights flight) {
-		flights[numOfFlights++] = flight;
-		return true;
-	}
-	
-	public void addFlight(Scanner s) {
-		addFlight(new flights(s));
-	}
-
-	public void sortFlights(Comparator<?> c) {
-		BubbleSort.bubbleSort(flights, c);
-	}
-
-	public flights[] getFlights() {
-		return flights;
-	}
-
-	public flights searchFlight(String flightNum) {
-		flights f = null;
-		for (int i = 0; i < flights.length; i++) {
-			if (flights[i].getFlightNum().equals(flightNum)) {
-				f = flights[i];
+	public Natbag(String fileName) throws FileNotFoundException {
+		Scanner s = new Scanner(new File(fileName));
+		if (s.hasNext()) {
+			int sizeOfArr = s.nextInt();
+			int num = 0;
+			this.allFlights = new flights[sizeOfArr];
+			while (s.hasNext()) {
+				allFlights[num++] = new flights(s);
+				numOfFlights++;
 			}
 		}
-		return f;
+		s.close();
 	}
 
 	public void save(String fileName) throws FileNotFoundException {
-		File f = new File(fileName);
-		PrintWriter pw = new PrintWriter(f);
-		pw.println(toString());
+		PrintWriter pw = new PrintWriter(new File(fileName));
+		pw.println(allFlights.length);
+		for (int i = 0; i < numOfFlights; i++) {
+				allFlights[i].save(pw);
+		}
 		pw.close();
+	}
+
+	public boolean addFlight(flights flight) {
+		for (int i = 0; i < numOfFlights; i++) {
+			if (allFlights[i].equals(flight)) {
+				return false;
+			}
+		}
+		allFlights[numOfFlights++] = new flights(flight);
+		return true;
+	}
+
+	public void sortFlights(Comparator<?> c) {
+		BubbleSort.bubbleSort(allFlights, c);
+	}
+
+	public flights[] getFlights() {
+		return allFlights;
+	}
+
+	public flights searchFlightByFlightNumber(String flightNum) throws Exception {
+		for (int i = 0; i < allFlights.length; i++) {
+			if (allFlights[i] != null) {
+				if (allFlights[i].getFlightNum().equalsIgnoreCase(flightNum)) {
+					return allFlights[i];
+				}
+			}
+		}
+		return null;
 	}
 
 	public String toString() {
 		String arr = "";
 		String dep = "";
-		for (int i = 0; i < flights.length; i++) {
-			if (flights[i] != null && flights[i].arriving) {
-				arr += flights[i].toString();
+		for (int i = 0; i < allFlights.length; i++) {
+			if (allFlights[i] != null && allFlights[i].arriving) {
+				arr += allFlights[i].toString();
 			}
 		}
-		for (int i = 0; i < flights.length; i++) {
-			if (flights[i] != null && !flights[i].arriving) {
-				dep += flights[i].toString();
+		for (int i = 0; i < allFlights.length; i++) {
+			if (allFlights[i] != null && !allFlights[i].arriving) {
+				dep += allFlights[i].toString();
 			}
 		}
 		return "Departing flights: \n" + dep + "\n" + "Arriving flights: \n" + arr;
 	}
-
 
 }
